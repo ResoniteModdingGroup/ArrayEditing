7ffd2702c53ef6f2c4f8f1883a02cef903f3fa83
Nytra <14206961+Nytra@users.noreply.github.com>
2024-09-06 11:39:01 +0100

    Initial commit

---
+++ MonkeyLoader.ModTemplate/BasicPatcher.cs
@@ -0,0 +1,29 @@
+﻿using FrooxEngine.ProtoFlux;
+using HarmonyLib;
+using MonkeyLoader.Patching;
+using MonkeyLoader.Resonite;
+using MonkeyLoader.Resonite.Features.FrooxEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.ModTemplate
+{
+    [HarmonyPatchCategory(nameof(BasicPatcher))]
+    [HarmonyPatch(typeof(ProtoFluxTool), nameof(ProtoFluxTool.OnAttach))]
+    internal class BasicPatcher : ResoniteMonkey<BasicPatcher>
+    {
+        // The options for these should be provided by your game's game pack.
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
+        {
+            yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
+        }
+
+        private static void Postfix()
+        {
+            Logger.Info(() => "Postfix for ProtoFluxTool.OnAttach()!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Mod Template for MonkeyLoader
+=============================
+
+A mod template, that shows the basic project setup for [MonkeyLoader](https://github.com/MonkeyModdingTroop/MonkeyLoader) mods.
